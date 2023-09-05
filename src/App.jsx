30f7fe39ea2df67fb9a2bddfcbbc3f08ff6fdcd2
Anton Shchukin <a.a.shchukin@gmail.com>
2023-09-05 16:23:46 +0700

    main to now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Main from "./pages/main/main.jsx";
+import Now from "./pages/now/now.jsx";
 import Promo from "./pages/promo/promo.jsx";
 import NotFound from "./pages/not-found/not-found.jsx";
 import ButtonDoc from "./documentation/button-doc/button-doc.jsx";
@@ -8,7 +8,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="" element={<Main/>}/>
+                <Route path="" element={<Now/>}/>
                 <Route path="/promo" element={<Promo/>}/>
 
                 <Route path="/button-doc" element={<ButtonDoc/>}/>
---
+++ src/pages/now/now.jsx
@@ -0,0 +1,10 @@
+function Now() {
+
+    return(
+        <div>
+            <h1>Now</h1>
+        </div>
+    )
+}
+
+export default Now
