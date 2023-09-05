f40298b9cd89d7958864069656c0f1bc852cb837
Anton Shchukin <a.a.shchukin@gmail.com>
2023-09-05 15:49:47 +0700

    nav links

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,29 +1,31 @@
+import {Link} from "react-router-dom";
+
 function Nav() {
     return (
         <nav className="nav">
             <div className="nav__floating-panel">
                 <div className="nav__wrap container">
                     <div className="nav__inner-wrap">
-                        <a className="nav__link nav__link--promo" href="promo.html">
+                        <Link className="nav__link nav__link--promo" to="/promo">
                             <img className="nav__icon" src="../symbols/nav-promo.svg" width="24" height="24" alt="" />
                             Акции
-                        </a>
-                        <a className="nav__link nav__link--now nav__link--current" href="now.html">
+                        </Link>
+                        <Link className="nav__link nav__link--now nav__link--current" to="/now">
                             <img className="nav__icon" src="../symbols/nav-now.svg" width="24" height="24" alt=""/>
                             Сейчас
-                        </a>
-                        <a className="nav__link nav__link--search" href="results.html">
+                        </Link>
+                        <Link className="nav__link nav__link--search" to="/results">
                             <img className="nav__icon" src="../symbols/nav-search.svg" width="24" height="24" alt=""/>
                             Поиск
-                        </a>
-                        <a className="nav__link nav__link--messages" href="dialogs.html">
+                        </Link>
+                        <Link className="nav__link nav__link--messages" to="/dialogs">
                             <img className="nav__icon" src="../symbols/nav-messages.svg" width="24" height="24" alt=""/>
                             Сообщения
-                        </a>
-                        <a className="nav__link nav__link--favorites" href="favorites.html">
+                        </Link>
+                        <Link className="nav__link nav__link--favorites" to="/favorites">
                             <img className="nav__icon" src="../symbols/nav-favorites.svg" width="24" height="24" alt=""/>
                             Посещу
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
