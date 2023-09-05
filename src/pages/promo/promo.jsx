21315ff0af0637188c7cc84eda84c26891a91526
Anton Shchukin <a.a.shchukin@gmail.com>
2023-09-05 15:36:22 +0700

    nav draft

--- src/pages/promo/promo.jsx
+++ src/pages/promo/promo.jsx
@@ -1,19 +1,23 @@
 import Deal from "../../components/deal/deal.jsx";
 import {deals} from "../../mocks/deals.js";
 import './../../pages/promo/promo.css';
+import Nav from "../../components/nav/nav.jsx";
 
 function Promo() {
 
     return (
-        <div className="promo container">
-            {
-                deals.map((deal) => {
-                    return (
-                        <Deal data={deal} key={deal.id}/>
-                    )
-                })
-            }
-        </div>
+        <>
+            <div className="promo container">
+                {
+                    deals.map((deal) => {
+                        return (
+                            <Deal data={deal} key={deal.id}/>
+                        )
+                    })
+                }
+            </div>
+            <Nav/>
+        </>
     )
 }
 
